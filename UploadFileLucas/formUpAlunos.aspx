02596a5af10b8f749f0da1b5797ab27cd5ac2e02
Ronaldo <armartinssistemas@gmail.com>
2018-04-22 14:38:16 -0300

    Inicial

---
+++ UploadFileLucas/formUpAlunos.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="formUpAlunos.aspx.cs" Inherits="UploadFileLucas.formUpAlunos" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:FileUpload ID="upFile" runat="server" />
+        </div>
+        <asp:Button ID="btnUpload" runat="server" OnClick="btnUpload_Click" Text="Upload Arquivo" />
+        <asp:Button ID="btnSalvarCadastro" runat="server" OnClick="btnSalvarCadastro_Click" Text="Salvar" />
+        <p>
+            <asp:GridView ID="grid" runat="server" AutoGenerateColumns="False" BackColor="White" BorderColor="#CC9966" BorderStyle="None" BorderWidth="1px" CellPadding="4" OnSelectedIndexChanged="grid_SelectedIndexChanged">
+                <Columns>
+                    <asp:BoundField DataField="id" HeaderText="ID" />
+                    <asp:BoundField DataField="ra" HeaderText="RA" />
+                    <asp:BoundField DataField="nome" HeaderText="Nome" />
+                    <asp:BoundField DataField="sobrenome" HeaderText="Sobrenome" />
+                    <asp:BoundField DataField="cidade" HeaderText="Cidade" />
+                    <asp:BoundField DataField="email" HeaderText="E-mail" />
+                    <asp:BoundField DataField="disciplina" HeaderText="Disciplina" />
+                </Columns>
+                <FooterStyle BackColor="#FFFFCC" ForeColor="#330099" />
+                <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="#FFFFCC" />
+                <PagerStyle BackColor="#FFFFCC" ForeColor="#330099" HorizontalAlign="Center" />
+                <RowStyle BackColor="White" ForeColor="#330099" />
+                <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="#663399" />
+                <SortedAscendingCellStyle BackColor="#FEFCEB" />
+                <SortedAscendingHeaderStyle BackColor="#AF0101" />
+                <SortedDescendingCellStyle BackColor="#F6F0C0" />
+                <SortedDescendingHeaderStyle BackColor="#7E0000" />
+            </asp:GridView>
+        </p>
+        <p>
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ UploadFileLucas/formUpAlunos.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Text;
+
+namespace UploadFileLucas
+{
+    public partial class formUpAlunos : System.Web.UI.Page
+    {
+        UnificadaEntities entityUni = new UnificadaEntities();
+
+       private static List<matricula> listaMatricula;
+
+        public static List<matricula> ListaMatricula { get => listaMatricula; set => listaMatricula = value; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpload_Click(object sender, EventArgs e)
+        {
+
+            upFile.SaveAs((@"C:\arquivos\" + upFile.FileName));
+
+            using (StreamReader sr = new StreamReader(@"C:\arquivos\" + upFile.FileName, Encoding.Default))
+            {
+
+                ListaMatricula = new List<matricula>();
+
+
+                if (sr.ReadLine() != null)
+                {
+                    String linha = sr.ReadLine();
+                    while (linha != null)
+                    {
+                        string[] array = linha.Split(',');
+                        matricula m = new UploadFileLucas.matricula();
+                        m.ra = array[0];
+                        //m.Senha = array[1];
+                        m.nome = array[2];
+                        m.sobrenome = array[3];
+                        // byte[] bytesCidade = Encoding.Default.GetBytes(array[4]);
+                        //String utf8Cidade = Encoding.UTF8.GetString(bytesCidade);
+                        //m.cidade = utf8Cidade;
+                        m.cidade = array[4];
+                        m.email = array[5];
+                        m.disciplina = array[6];
+                        //m.Chave = array[7];
+                        //m.Departamento = array[8];
+                        m.curso = array[9];
+                        ListaMatricula.Add(m);
+                        linha = sr.ReadLine();
+                    }
+                    grid.DataSource = ListaMatricula;
+                    grid.DataBind();
+                }
+            }
+
+        }
+
+        protected void grid_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSalvarCadastro_Click(object sender, EventArgs e)
+        {
+            // Salvar lista de cadastro
+            entityUni.matriculas.AddRange(ListaMatricula);
+            entityUni.SaveChanges();
+
+        }
+    }
+}
